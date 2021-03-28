9c9c00f68710588bc7bd4064a525c829eef9258d
Константин Худышка <k.iroshnikov@itmegastar.com>
2021-03-28 14:59:19 +0700

    ячейка с прозрачностью

--- lib/Views/CellWord.dart
+++ lib/Views/CellWord.dart
@@ -3,7 +3,9 @@ import 'package:english_dictionary_flutter/Models/Word.dart';
 import 'package:english_dictionary_flutter/Support/Const.dart';
 import 'package:flutter/material.dart';
 
-class CellWord extends StatelessWidget {
+
+class CellWord extends StatefulWidget {
+
   Word word;
   bool rusWay;
   bool hideTarnslate;
@@ -11,6 +13,17 @@ class CellWord extends StatelessWidget {
   Function(Word) presedWord;
 
   CellWord({@required this.word, @required this.rusWay, @required this.hideTarnslate});
+
+
+
+  @override
+  _CellWordState createState() => _CellWordState();
+}
+
+
+
+class _CellWordState extends State<CellWord> {
+
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +52,7 @@ class CellWord extends StatelessWidget {
 
   Row get _hederCell {
 
-    final favorit = word.favorit ?? false;
+    final favorit = widget.word.favorit ?? false;
 
     final imageName = favorit ? "assets/icons/favorit.png" : "assets/icons/not_favorit.png";
 
@@ -52,7 +65,7 @@ class CellWord extends StatelessWidget {
           width: Const.wDevice - 50,
           child: Padding(
           padding: EdgeInsets.only(left: 20, top: 0, right: 0, bottom: 0),
-          child: Text(rusWay ? word.rusValue : word.engValue,
+          child: Text(widget.rusWay ? widget.word.rusValue : widget.word.engValue,
             textAlign: TextAlign.left,
             style: TextStyle(color: Colors.black, fontSize: 32, fontWeight: FontWeight.bold),
           ),
@@ -87,12 +100,16 @@ class CellWord extends StatelessWidget {
     );
   }
 
-  Container get _containerFooter{
 
-    String translate = rusWay ? word.engValue : word.rusValue;
-    Color colorText = hideTarnslate ? Const.clearColor : Colors.black;
 
-    final descr = word.descript;
+
+
+  Container get _containerFooter {
+
+    String translate = widget.rusWay ? widget.word.engValue : widget.word.rusValue;
+    Color colorText = widget.hideTarnslate ? Const.clearColor : Colors.black;
+
+    final descr = widget.word.descript;
     if (descr != ""){
       translate = translate + "/n/n$descr";
     }
@@ -109,5 +126,32 @@ class CellWord extends StatelessWidget {
         )
     );
   }
+
+  Container get _containerBlack {
+
+    String translate = widget.rusWay ? widget.word.engValue : widget.word.rusValue;
+    Color colorText = widget.hideTarnslate ? Const.clearColor : Colors.black;
+
+    final descr = widget.word.descript;
+    if (descr != ""){
+      translate = translate + "/n/n$descr";
+    }
+
+
+    return Container(
+      width: double.infinity,
+      child: Padding(
+          padding: EdgeInsets.only(left: 20, top: 7, right: 16, bottom: 10),
+          child: Text(translate,
+            textAlign: TextAlign.left,
+            style: TextStyle(color: colorText, fontSize: 23, fontWeight: FontWeight.w500),
+          ),
+        )
+    );
+  }
+
+  
+
+
 
 }
