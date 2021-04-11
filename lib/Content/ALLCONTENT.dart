5f07a39fd0a33e83f3848988f5604c55e03c6901
Константин Худышка <k.iroshnikov@itmegastar.com>
2021-04-11 19:15:10 +0700

    готовлю к показу лаунчу на андроиде

--- lib/Content/ALLCONTENT.dart
+++ lib/Content/ALLCONTENT.dart
@@ -3,9 +3,9 @@ import 'package:english_dictionary_flutter/export.dart';
 
 class AllContent {
   static List<Map<String, dynamic>> allContent = [
-    // Prepositions.allWord,
-    // ProfessionAndWork.allWord,
-    // Subjects.allWord,
+    Prepositions.allWord,
+    ProfessionAndWork.allWord,
+    Subjects.allWord,
     Time.allWord
   ];
 }
